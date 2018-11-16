3e77b32692f0229fc5bbbab8afb0b23df559be37
AWA5CG8126BTQ\Administrator <petergo2school@gmail.com>
2018-11-16 13:32:47 +0100

    Bla bla

--- testingGit/testingGit/Program.cs
+++ testingGit/testingGit/Program.cs
@@ -7,6 +7,9 @@ namespace testingGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("ggggg");
+            global::System.Console.WriteLine("hhjhhhh");
+            global::System.Console.WriteLine();
         }
     }
 }
